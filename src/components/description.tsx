43d8869c58efa63fee738abb6895e2c4ceb4dacc
Daffa <daffamfzn@gmail.com>
2023-12-25 00:00:55 +0700

    feat: initial commit

---
+++ src/components/description.tsx
@@ -0,0 +1,7 @@
+export default function Description() {
+    return (
+        <div className="min-h-screen w-full flex flex-col items-center justify-center">
+            <span>I'm a Software Engineer</span>
+        </div>
+    )
+}
---
+++ src/components/intro.tsx
@@ -0,0 +1,8 @@
+export default function Intro() {
+    return(
+        <div className="min-h-screen w-full flex flex-col items-center justify-center">
+            <img></img>
+            <span className="text-2xl font-semibold">Hi, I'm Daffa</span>
+        </div>
+    )
+}
