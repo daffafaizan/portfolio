70394a6d7f49b09b7c05a9fea51f8fa3c65ea968
Daffa <daffamfzn@gmail.com>
2023-12-30 02:02:07 +0700

    feat: changed intro

--- src/components/main/intro.tsx
+++ src/components/main/intro.tsx
@@ -1,11 +1,10 @@
 export default function Intro() {
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-center">
-      <div className="flex flex-col justify-center text-center gap-y-1">
-        <span className="text-4xl font-semibold text-[#45b8ac]">
-          Daffa M. Faizan
-        </span>
-        <span className="text-2xl">Software Engineer</span>
+      <div className="flex flex-col justify-center text-3xl text-left gap-y-1">
+        <span className="text-gray-300">University of Indonesia</span>
+        <span className="font-semibold">Daffa M. Faizan</span>
+        <span className="text-gray-300">Software Engineer</span>
       </div>
     </div>
   );
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,10 +1,15 @@
+import { Bars3Icon } from "@heroicons/react/24/outline";
+
 export default function Navbar() {
   return (
-    <div className="fixed top-0 w-full p-5">
+    <div className="fixed flex flex-row items-center justify-between top-0 w-full px-7 py-5">
       <div className="flex flex-col justify-center items-center">
-        <span className="text-sm mr-auto">
-          dmf<span className="text-sm text-[#45b8ac]">.</span>
+        <span className="text-sm">
+          dmf<span className="text-sm text-[#45b8ac]">.</span>{" "}
         </span>
+      </div>
+      <div>
+        <Bars3Icon className="block h-6 w-6" />
       </div>
     </div>
   );
