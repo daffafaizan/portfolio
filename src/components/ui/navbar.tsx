f4d95040585fcc86accce73be5bb7be2f2dad43c
Daffa <daffamfzn@gmail.com>
2023-12-30 01:17:13 +0700

    feat: installed heroicons

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <div className="fixed top-0 w-full p-3">
+    <div className="fixed top-0 w-full p-5">
       <div className="flex flex-col justify-center items-center">
         <span className="text-sm mr-auto">
           dmf<span className="text-sm text-[#45b8ac]">.</span>
