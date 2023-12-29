c899c195ecc81b14a5b4c8932662b76cecf11ba7
Daffa <daffamfzn@gmail.com>
2023-12-30 01:10:46 +0700

    feat: added logo at navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/main/intro";
 import Description from "@/components/main/description";
+import Navbar from "@/components/ui/navbar";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center p-4">
+      <Navbar />
       <Intro />
       <Description />
     </main>
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,3 +1,11 @@
 export default function Navbar() {
-  return <div></div>;
+  return (
+    <div className="fixed top-0 w-full p-3">
+      <div className="flex flex-col justify-center items-center">
+        <span className="text-sm mr-auto">
+          dmf<span className="text-sm text-[#45b8ac]">.</span>
+        </span>
+      </div>
+    </div>
+  );
 }
