bbe3ef98bcf4269180907c83ae45b74e85cdd755
Daffa <daffamfzn@gmail.com>
2023-12-29 23:21:15 +0700

    feat: added picture

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,8 +1,7 @@
 export default function Intro() {
-    return(
-        <div className="min-h-screen w-full flex flex-col items-center justify-center">
-            <img></img>
-            <span className="text-2xl font-semibold">Hi, I'm Daffa</span>
-        </div>
-    )
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center justify-center">
+      <span className="text-2xl font-semibold">Hi, I'm Daffa</span>
+    </div>
+  );
 }
