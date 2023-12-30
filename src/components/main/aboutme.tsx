3da43c748cf567044885d00bc5e98128d156f4df
Daffa <daffamfzn@gmail.com>
2023-12-30 21:39:35 +0700

    style: added style for sm and above @aboutme

--- src/components/main/aboutme.tsx
+++ src/components/main/aboutme.tsx
@@ -1,11 +1,11 @@
 export default function AboutMe() {
   return (
-    <div className="min-h-screen w-full flex flex-col justify-center items-center py-4">
-      <div className="w-full text-left px-2">
-        <span className="text-3xl font-semibold">About me</span>
+    <div className="min-h-screen w-full flex flex-col justify-center items-center px-2 py-4">
+      <div className="w-full sm:w-4/5 text-left">
+        <span className="text-3xl sm:text-5xl font-semibold">About me</span>
       </div>
-      <div className="w-full text-justify px-2 mt-4">
-        <span className="text-lg">
+      <div className="w-full sm:w-4/5 text-justify mt-4">
+        <span className="text-lg sm:text-2xl">
           Daffa, currently a third-year Computer Science Undergraduate at the
           University of Indonesia, has developed a strong interest in Software
           Engineering, specifically focusing on Front End Development. Known for
@@ -15,8 +15,8 @@ export default function AboutMe() {
           into Back End Development to achieve a well-rounded proficiency.
         </span>
       </div>
-      <div className="w-full px-2 mt-4">
-        <button className="border border-black rounded-full px-3 py-2">
+      <div className="w-full sm:w-4/5 mt-4">
+        <button className="border border-black rounded-full px-3 py-2 text-base sm:text-xl">
           Get to know me.
         </button>
       </div>
