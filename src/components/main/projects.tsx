09b43e799dc544678bb89ed813bc015a052a8a33
Daffa <daffamfzn@gmail.com>
2023-12-30 18:51:27 +0700

    feat: finished projects section on home page

--- src/components/main/projects.tsx
+++ src/components/main/projects.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import pomodoro from "../../../public/Pomodoro/Shot.png";
+import invite from "../../../public/Invite/Scene 1.png";
+import nubengine from "../../../public/Nubengine/MacBook-Pro-16 (1).png";
 
 export default function Projects() {
   return (
@@ -18,7 +20,41 @@ export default function Projects() {
         <div className="absolute bottom-5 right-5 px-3 py-2 bg-white rounded-2xl text-sm">
           <div className="flex flex-row justify-between items-center gap-2">
             <span className="font-semibold">Pomodoro</span>
-            <button className="px-2 py-1 bg-green-400 text-white rounded-xl">
+            <button className="px-2 py-1 bg-green-400 font-medium text-white rounded-xl">
+              <span>Open</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="relative w-full px-2 mt-4">
+        <Image
+          className="rounded-2xl"
+          src={invite}
+          alt="Invite"
+          width={500}
+          height={500}
+        />
+        <div className="absolute bottom-5 right-5 px-3 py-2 bg-white rounded-2xl text-sm">
+          <div className="flex flex-row justify-between items-center gap-2">
+            <span className="font-semibold">Invite</span>
+            <button className="px-2 py-1 bg-green-400 font-medium text-white rounded-xl">
+              <span>Open</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="relative w-full px-2 mt-4">
+        <Image
+          className="rounded-2xl"
+          src={nubengine}
+          alt="Nubengine"
+          width={500}
+          height={500}
+        />
+        <div className="absolute bottom-5 right-5 px-3 py-2 bg-white rounded-2xl text-sm">
+          <div className="flex flex-row justify-between items-center gap-2">
+            <span className="font-semibold">Nubengine</span>
+            <button className="px-2 py-1 bg-green-400 font-medium text-white rounded-xl">
               <span>Open</span>
             </button>
           </div>
