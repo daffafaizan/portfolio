c7813dabf9271c71d3b0d5880491ebeacefe2ec9
Daffa <daffamfzn@gmail.com>
2023-12-30 19:09:54 +0700

    feat: added new project

--- src/components/main/projects.tsx
+++ src/components/main/projects.tsx
@@ -2,12 +2,35 @@ import Image from "next/image";
 import pomodoro from "../../../public/Pomodoro/Shot.png";
 import invite from "../../../public/Invite/Scene 1.png";
 import nubengine from "../../../public/Nubengine/MacBook-Pro-16 (1).png";
+import todolist from "../../../public/Todolist/Shot (14).png";
 
 export default function Projects() {
   return (
     <div className="min-h-screen w-full flex flex-col justify-center items-center py-4">
       <div className="w-full text-left px-2">
         <span className="text-3xl font-semibold">Projects</span>
+      </div>
+      <div className="relative w-full px-2 mt-4">
+        <Image
+          className="rounded-2xl"
+          src={todolist}
+          alt="Todolist"
+          width={500}
+          height={500}
+        />
+        <div className="absolute bottom-5 right-5 px-3 py-2 bg-white rounded-2xl text-sm">
+          <div className="flex flex-row justify-between items-center gap-2">
+            <span className="font-semibold">Todolist</span>
+            <a
+              className="px-2 py-1 bg-green-400 font-medium text-white rounded-xl"
+              href="https://todolist.daffafaizan.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span>Open</span>
+            </a>
+          </div>
+        </div>
       </div>
       <div className="relative w-full px-2 mt-4">
         <Image
