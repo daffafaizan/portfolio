e4ea27b1f7628a5927bd10a3f38130d6395b7648
Daffa <daffamfzn@gmail.com>
2023-12-30 18:11:07 +0700

    feat: added project mockups

--- src/components/main/projects.tsx
+++ src/components/main/projects.tsx
@@ -1,10 +1,13 @@
-import CarouselDefault from "./carousel";
+import Image from "next/image";
 
 export default function Projects() {
   return (
     <div className="min-h-screen w-full flex flex-col justify-center items-center py-4">
       <div className="w-full text-left px-2">
         <span className="text-3xl font-semibold">Projects</span>
+      </div>
+      <div>
+        <Image src="" alt="" />
       </div>
       <div className="w-full px-2 mt-4">
         <button className="border border-black rounded-full px-3 py-2">
