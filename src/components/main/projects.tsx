4944b45b68dc57c3ee53fe7f82e05c3fbf6ecadf
Daffa <daffamfzn@gmail.com>
2023-12-30 18:34:01 +0700

    feat: added project card and floating desc

--- src/components/main/projects.tsx
+++ src/components/main/projects.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import pomodoro from "../../../public/Pomodoro/Shot.png";
 
 export default function Projects() {
   return (
@@ -6,8 +7,22 @@ export default function Projects() {
       <div className="w-full text-left px-2">
         <span className="text-3xl font-semibold">Projects</span>
       </div>
-      <div>
-        <Image src="" alt="" />
+      <div className="relative w-full px-2 mt-4">
+        <Image
+          className="rounded-2xl"
+          src={pomodoro}
+          alt="Pomodoro"
+          width={500}
+          height={500}
+        />
+        <div className="absolute bottom-5 right-5 px-3 py-2 bg-white rounded-2xl text-sm">
+          <div className="flex flex-row justify-between items-center gap-2">
+            <span className="font-semibold">Pomodoro</span>
+            <button className="px-2 py-1 bg-green-400 text-white rounded-xl">
+              <span>Open</span>
+            </button>
+          </div>
+        </div>
       </div>
       <div className="w-full px-2 mt-4">
         <button className="border border-black rounded-full px-3 py-2">
