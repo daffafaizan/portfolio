e9fc3d15304a992dc0b5578debb38c5578f0d1d5
Daffa <daffamfzn@gmail.com>
2023-12-30 19:05:16 +0700

    style: minor navbar style change

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -2,7 +2,7 @@ import { Bars3Icon } from "@heroicons/react/24/outline";
 
 export default function Navbar() {
   return (
-    <div className="fixed flex flex-row items-center justify-between top-0 w-full px-7 py-5">
+    <div className="fixed flex flex-row items-center justify-between top-0 z-20 w-full px-7 py-5 bg-white">
       <div className="flex flex-col justify-center items-center">
         <span className="text-sm">
           dmf<span className="text-sm text-[#45b8ac]">.</span>{" "}
