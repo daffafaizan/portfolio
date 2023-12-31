d399b9ea6bceaefc37b3f4d1e3d4fabc94ea72da
Daffa <daffamfzn@gmail.com>
2023-12-31 11:57:25 +0700

    feat: added functional navbar for md screens and above

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,6 +1,15 @@
-import { Bars3Icon } from "@heroicons/react/24/outline";
+"use client";
+
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 export default function Navbar() {
+  const pathName = usePathname();
+  const [showNavbar, setShowNavbar] = useState(false);
+  useEffect(() => {
+    console.log(pathName);
+  });
   return (
     <div className="fixed flex flex-row items-center justify-between top-0 z-20 w-full px-7 py-5 bg-white">
       <div className="flex flex-col justify-center items-center">
@@ -10,10 +19,42 @@ export default function Navbar() {
           </span>
         </a>
       </div>
-      <div>
-        <button>
-          <Bars3Icon className="block h-6 w-6 sm:h-8 sm:w-8" />
-        </button>
+      <div className="hidden md:flex flex-row justify-center items-center text-xl gap-6">
+        {pathName !== "/" ? (
+          <div>
+            <a href="/">
+              <span>Home</span>
+            </a>
+          </div>
+        ) : (
+          <></>
+        )}
+        <div>
+          <a href="/projects">
+            <span>Projects</span>
+          </a>
+        </div>
+        <div>
+          <a href="/blog">
+            <span>Blog</span>
+          </a>
+        </div>
+        <div>
+          <a href="/me">
+            <span>About me</span>
+          </a>
+        </div>
+      </div>
+      <div className="md:hidden flex flex-col justify-center items-center">
+        {showNavbar ? (
+          <button onClick={() => setShowNavbar(!showNavbar)}>
+            <Bars3Icon className="block h-6 w-6 sm:h-8 sm:w-8" />
+          </button>
+        ) : (
+          <button onClick={() => setShowNavbar(!showNavbar)}>
+            <XMarkIcon className="block h-6 w-6 sm:h-8 sm:w-8" />
+          </button>
+        )}
       </div>
     </div>
   );
