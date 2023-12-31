b6a35332c001dcdfbfb6b4a63396e1d0fd6357ad
Daffa <daffamfzn@gmail.com>
2023-12-31 08:20:30 +0700

    feat: added new page and relocated navbar and footer

---
+++ src/app/me/page.tsx
@@ -0,0 +1,7 @@
+export default function Me() {
+  return (
+    <main className="flex flex-col items-center p-4">
+      <span>test</span>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import AboutMe from "@/components/main/aboutme";
 import Intro from "@/components/main/intro";
 import Projects from "@/components/main/projects";
-import Footer from "@/components/ui/footer";
-import Navbar from "@/components/ui/navbar";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center p-4">
-      <Navbar />
       <Intro />
       <AboutMe />
       <Projects />
-      <Footer />
     </main>
   );
 }
