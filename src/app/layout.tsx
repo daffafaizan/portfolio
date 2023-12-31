8c5723d47fc4bc7121b8ba1ca2d81182961a4a07
Daffa <daffamfzn@gmail.com>
2023-12-31 12:48:20 +0700

    feat: added tab icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="../../public/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
