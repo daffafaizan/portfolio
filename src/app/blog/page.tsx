01fd97ea338df88f584cc9c6b3d4799c4beef174
Daffa <daffamfzn@gmail.com>
2023-12-31 11:58:33 +0700

    feat: added blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import UnderConstruction from "@/components/misc/construction";
+
+export default function Blog() {
+  return (
+    <main className="flex flex-col items-center p-4">
+      <UnderConstruction />
+    </main>
+  );
+}
