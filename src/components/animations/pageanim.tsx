d4a9526ec64fa7e906ae4f504768bc37c72fc0e0
Daffa <daffamfzn@gmail.com>
2024-01-02 01:00:22 +0700

    feat: added ease and duration transition

--- src/components/animations/pageanim.tsx
+++ src/components/animations/pageanim.tsx
@@ -1,37 +1,48 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import { AnimatePresence, Spring, motion } from "framer-motion";
 
-const transitionSpringPhysics: Spring = {
-  type: "spring",
-  mass: 0.2,
-  stiffness: 80,
-  damping: 10,
-};
-function AnimatedPage({ children }: { children: any }) {
+export default function AnimatedPage({ children }: { children: any }) {
+  const pathName = usePathname();
+  const transitionSpringPhysics: Spring = {
+    type: "spring",
+    mass: 0.2,
+    stiffness: 80,
+    damping: 10,
+  };
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   return (
     <AnimatePresence mode="wait">
       <motion.div>
-        <motion.div
-          className="fixed w-screen z-50 bottom-0 bg-black"
-          transition={transitionSpringPhysics}
-          animate={{ height: "0vh" }}
-          exit={{ height: "100vh" }}
-        />
-        <motion.div
-          className="fixed w-screen z-50 top-0 bg-blue-200"
-          transition={transitionSpringPhysics}
-          initial={{ height: "100vh" }}
-          animate={{ height: "0vh", transition: { delay: 0.2 } }}
-        />
+        {pathName !== "/" ? (
+          <motion.div
+            className="fixed w-screen z-50 top-0 bg-blue-200"
+            transition={transitionSpringPhysics}
+            initial={{ height: "100vh" }}
+            animate={{
+              height: "0vh",
+              transition: { duration: 1.5, ease: [0.87, 0, 0.13, 1] },
+            }}
+          />
+        ) : (
+          <motion.div
+            className="fixed w-screen z-50 top-0 bg-blue-200"
+            transition={transitionSpringPhysics}
+            initial={{ height: "100vh" }}
+            animate={{
+              height: "0vh",
+              transition: { duration: 1.5, ease: [0.87, 0, 0.13, 1] },
+            }}
+          />
+        )}
+
         {children}
       </motion.div>
     </AnimatePresence>
   );
 }
-
-export default AnimatedPage;
