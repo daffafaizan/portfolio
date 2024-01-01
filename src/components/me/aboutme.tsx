c454699c291326c4c97ebea4472359f8a5731b1b
Daffa <daffamfzn@gmail.com>
2024-01-01 09:30:02 +0700

    style: minor style addition

--- src/components/me/aboutme.tsx
+++ src/components/me/aboutme.tsx
@@ -3,7 +3,7 @@ import profile from "../../../public/new_pp_large.jpg";
 
 export default function AboutMe() {
   return (
-    <div className="min-h-screen w-full sm:w-5/6 flex flex-col lg:flex-row justify-stretch items-stretch mt-16 px-2 pb-4 lg:mt-0 lg:pt-4 gap-4">
+    <div className="min-h-screen w-full sm:w-5/6 flex flex-col lg:flex-row justify-stretch items-stretch mt-16 px-2 pb-4 lg:mt-0 lg:pt-4 gap-4 lg:gap-6">
       <div className="w-full lg:w-5/12 flex flex-col justify-center items-center">
         <Image
           className="aspect-square object-cover rounded-full h-[200px] w-[200px] sm:h-60 sm:w-60 lg:h-80 lg:w-80 xl:h-96 xl:w-96"
