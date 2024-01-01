929a79ced5a824f61d33ff5b41cc443ad1d4fd41
Daffa <daffamfzn@gmail.com>
2024-01-02 01:27:19 +0700

    feat: finished page transition

--- src/components/animations/pageanim.tsx
+++ src/components/animations/pageanim.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import { AnimatePresence, Spring, motion } from "framer-motion";
 
 export default function AnimatedPage({ children }: { children: any }) {
-  const pathName = usePathname();
   const transitionSpringPhysics: Spring = {
     type: "spring",
     mass: 0.2,
@@ -19,28 +17,15 @@ export default function AnimatedPage({ children }: { children: any }) {
   return (
     <AnimatePresence mode="wait">
       <motion.div>
-        {pathName !== "/" ? (
-          <motion.div
-            className="fixed w-screen z-50 top-0 bg-blue-200"
-            transition={transitionSpringPhysics}
-            initial={{ height: "100vh" }}
-            animate={{
-              height: "0vh",
-              transition: { duration: 1.5, ease: [0.87, 0, 0.13, 1] },
-            }}
-          />
-        ) : (
-          <motion.div
-            className="fixed w-screen z-50 top-0 bg-blue-200"
-            transition={transitionSpringPhysics}
-            initial={{ height: "100vh" }}
-            animate={{
-              height: "0vh",
-              transition: { duration: 1.5, ease: [0.87, 0, 0.13, 1] },
-            }}
-          />
-        )}
-
+        <motion.div
+          className="fixed flex justify-center items-center w-screen z-50 top-0 bg-[#F5F5F5]"
+          transition={transitionSpringPhysics}
+          initial={{ height: "100vh" }}
+          animate={{
+            height: "0vh",
+            transition: { duration: 1.5, ease: [0.87, 0, 0.13, 1] },
+          }}
+        />
         {children}
       </motion.div>
     </AnimatePresence>
