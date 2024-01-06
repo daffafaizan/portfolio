b041751afeea09b3f1923d9d7ba02055338c0bdc
Daffa <daffamfzn@gmail.com>
2024-01-06 21:32:38 +0700

    feat: added allblogs page

---
+++ src/components/blog/allblogs.tsx
@@ -0,0 +1,9 @@
+import AnimatedDiv from "../animations/animateddiv";
+
+export default function AllBlogs() {
+  return (
+    <AnimatedDiv className="min-h-screen w-full sm:w-5/6 flex flex-col justify-center items-center px-2 py-4">
+      test
+    </AnimatedDiv>
+  );
+}
