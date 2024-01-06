b9d10a188a512202bb1a52d2418917efa5b22f18
Daffa <daffamfzn@gmail.com>
2024-01-06 21:37:41 +0700

    feat: successfully fetched posts from backend

--- src/components/blog/allblogs.tsx
+++ src/components/blog/allblogs.tsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
 import AnimatedDiv from "../animations/animateddiv";
 
 export default function AllBlogs() {
+  const url = "http://localhost:8080/api/posts";
+  const [posts, setPosts] = useState([]);
+  const getPosts = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
   return (
     <AnimatedDiv className="min-h-screen w-full sm:w-5/6 flex flex-col justify-center items-center px-2 py-4">
       test
