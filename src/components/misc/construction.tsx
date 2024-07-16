46e5ed767446c4f15201e9cf8b0b1e15c186f9b9
Daffa <daffamfzn@gmail.com>
2024-07-17 00:39:51 +0700

    feat: deactivated blog page

--- src/components/misc/construction.tsx
+++ src/components/misc/construction.tsx
@@ -5,7 +5,7 @@ export default function UnderConstruction() {
   return (
     <AnimatedDiv className="min-h-screen w-full sm:w-5/6 flex flex-col justify-center items-center px-2 py-4">
       <div>
-        <span className="text-3xl sm:text-5xl lg:text-7xl font-semibold">
+        <span className="text-3xl text-white sm:text-5xl lg:text-7xl font-semibold">
           Work in progress!
         </span>
       </div>
